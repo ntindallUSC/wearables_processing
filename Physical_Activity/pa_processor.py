e8aee9e77c4abde1dfffbaf0aad50edf19f8266a
ntindallUSC <ntindall@email.sc.edu>
2022-06-06 08:40:33 -0400

    -Added Apple Path collection

--- Physical_Activity/pa_processor.py
+++ Physical_Activity/pa_processor.py
@@ -6,6 +6,7 @@ align it.
 """
 import tkinter as tk
 from tkinter import filedialog
+import os
 import glob
 
 # This is used to intialize the tkinter interface where the user selects the PA Participant Folder
@@ -15,7 +16,25 @@ root.withdraw()
 
 print("PLease select the PA Participant you wish to process")
 pa_path = filedialog.askdirectory() # This line gets the path of the directory
+particpant_num = pa_path[-4:]
+print(f'Participant Number {particpant_num}')
 
-# Now that the path of the director I need to read in the data from each device and process the data.
+# Now that the path of the director I need to read in and process the following devices:
+
+# APPLE WATCH Processing
+# First get the path of the Apple Watch Data files
+apple_path = pa_path + '/Apple Data'
+
+# Apple Watch has 2 types of data output files: Accelerometer and Heart rate. Need to grab both
+if os.path.isdir(apple_path):
+    # Get list of acceleration files
+    accel_files = glob.glob(apple_path + '/*_sl.csv')
+    print(f"Apple Sensorlog Files :\n{accel_files}")
+    # Get list of heart rate files
+    hr_files = glob.glob(apple_path + '/*_hr.csv')
+    print(f"Cardiogram Files :\n{hr_files}")
+
+
+
 
 
