2b016454a66c115801b8f7d6c734ba48d4259dde
ntindallUSC <ntindall@email.sc.edu>
2023-01-10 09:19:54 -0500

    - Adjusted code to run if missing actigraph data

--- Physical_Activity/pa_processor.py
+++ Physical_Activity/pa_processor.py
@@ -215,7 +215,7 @@ def process_participant(pa_path, v_drive):
     hr_path = pa_path + "/" + particpant_num
     plot_hr(aligned_df, ["Actiheart"] + devices, trial_start, trial_end, activities, hr_path, k5_path)
     print("Plotting Accelerometers")
-    if actigraph_data.empty:
+    if actigraph_data.empty():
         plot_accel(agg_df, devices, trial_start, trial_end, activities, pa_path + "/" + particpant_num)
     else :
         plot_accel(agg_df, ["Actigraph"] + devices, trial_start, trial_end, activities, pa_path + "/" + particpant_num)
