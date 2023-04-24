f9938b926480cb8d554f96bbc755fed6f6d3d09b
ntindallUSC <ntindall@email.sc.edu>
2023-04-24 11:37:08 -0400

    - now add notes from fl daily diary to wear log

--- Protocol_Scripts/process_protocol.py
+++ Protocol_Scripts/process_protocol.py
@@ -210,7 +210,11 @@ def process_participant(in_path, v_drive, protocol='PA'):
         shift_files = glob.glob(actiheart_path + "*shift.txt")
         devices.append(process_actiheart(actiheart_files, shift_files, actiheart_path, participant_num, participant_age,
                                          trial_start, trial_end, protocol))
-        plot_actiheart_hr(devices[-1][1], actiheart_path + "/Processed Data/" + participant_num + "_" + protocol.split("-")[1] + '_hr.png')
+        if protocol[:2] == "FL":
+            plot_actiheart_hr(devices[-1][1], actiheart_path + "/Processed Data/" + participant_num + "_" + protocol.split("-")[1] + '_hr.png')
+        else:
+            plot_actiheart_hr(devices[-1][1],
+                              actiheart_path + "/Processed Data/" + participant_num + '_hr.png')
 
     # Process the PSG Files
     psg_path = in_path + "/PSG/"
